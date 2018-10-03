8dafbeb4bf063c427b2fab582ae85f7f62ffaf47
Marco Craveiro <marco.craveiro@gmail.com>
2018-10-03 18:12:34 +0100

    fixes to enable dogen generation of models

--- build/cmake/FindDogen.cmake
+++ build/cmake/FindDogen.cmake
@@ -16,6 +16,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
+
+#
+# Set CMAKE_PROGRAM_PATH as required
 #
 find_program(DOGEN_KNITTER_EXECUTABLE NAMES dogen.knitter
     HINTS ${CMAKE_BINARY_DIR}/stage/bin)
