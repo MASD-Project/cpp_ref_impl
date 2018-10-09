591595aa2c34213edf4320c5bdd6951c83b0d623
Marco Craveiro <marco.craveiro@gmail.com>
2018-10-09 20:37:00 +0100

    build: move dependencies to vcpkg
    - upgrade to clang 7

--- build/scripts/travis.install.linux.sh
+++ build/scripts/travis.install.linux.sh
@@ -19,20 +19,18 @@
 #
 
 #
-# deps
+# vcpkg dependencies
 #
-dropbox="https://www.dropbox.com/s/lco0kn2wsjbmpjf/hedgr_deps_ubuntu_14.04_amd64_v1.tgz?dl=0"
-version=1
-package="hedgr_deps_ubuntu_14.04_amd64_v${version}.tgz"
-input_location="${dropbox}/${package}"
-output_location="/tmp/${package}"
-extract_dir="/tmp/dogen_deps"
-wget --no-check-certificate ${input_location} -O ${output_location}
-mkdir ${extract_dir}
-tar -xaf ${output_location} -C ${extract_dir}
-rm ${output_location}
-echo "Deleted ${output_location}"
-ls ${extract_dir}/*
+vcpkg_folder="vcpkg-export-20181009-164107"
+vcpkg_package="${vcpkg_folder}.zip"
+vcpkg_input_location="https://www.dropbox.com/s/1oqktqwtprt4cfu/${vcpkg_package}?dl=0"
+vcpkg_output_location="/tmp/${vcpkg_package}"
+vcpkg_extract_dir="/tmp/"
+vcpkg_final_folder="vcpkg-export"
+wget --no-check-certificate ${vcpkg_input_location} -O ${vcpkg_output_location}
+unzip -q ${vcpkg_output_location} -d ${vcpkg_extract_dir}
+mv ${vcpkg_extract_dir}/${vcpkg_folder} ${vcpkg_extract_dir}/${vcpkg_final_folder}
+ls ${vcpkg_extract_dir}/${vcpkg_final_folder}
 
 #
 # common repos
@@ -44,25 +42,27 @@ sudo apt-get update -qq
 # clang
 #
 if [ "$CXX" == "clang++" ]; then
+    version="7"
     server="http://apt.llvm.org/trusty/"
     sources="/etc/apt/sources.list"
     trusty="llvm-toolchain-trusty"
-    sudo sh -c "echo 'deb ${server} ${trusty}-6.0 main' >> ${sources}"
-    sudo sh -c "echo 'deb ${server} ${trusty}-6.0 main' >> ${sources}"
+    sudo sh -c "echo 'deb ${server} ${trusty}-${version} main' >> ${sources}"
+    sudo sh -c "echo 'deb ${server} ${trusty}-${version} main' >> ${sources}"
     wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-get update -qq
 
-    sudo apt-get install --allow-unauthenticated -qq clang-6.0
-    which clang-6.0
-    export CXX="clang++-6.0" CC="clang-6.0"
+    sudo apt-get install --allow-unauthenticated -qq clang-${version}
+    which clang-${version}
+    export CXX="clang++-${version}" CC="clang-${version}"
 fi
 
 #
 # g++
 #
 if [ "$CXX" = "g++" ]; then
-    sudo apt-get install -qq g++-8
-    export CXX="g++-8" CC="gcc-8" GCOV="gcov-8"
+    version="8"
+    sudo apt-get install -qq g++-${version}
+    export CXX="g++-${version}" CC="gcc-${version}" GCOV="gcov-${version}"
 fi
 
 #
@@ -73,16 +73,18 @@ sudo apt-get install ninja-build
 #
 # cmake
 #
-cmake_name="cmake-3.4.1-Linux-x86_64"
+sudo rm -rf /usr/local/cmake-3.9.2
+
+cmake_name="cmake-3.12.0-Linux-x86_64"
 cmake_tar="${cmake_name}.tar.gz"
-cmake_input="https://cmake.org/files/v3.4/${cmake_tar}"
+cmake_input="https://cmake.org/files/v3.12/${cmake_tar}"
 cmake_output="/tmp/${cmake_tar}"
 wget --no-check-certificate ${cmake_input} -O ${cmake_output}
 tar xf ${cmake_output} -C /tmp
 sudo cp -r /tmp/${cmake_name}/* /usr
+which cmake
+cmake --version
 rm -rf ${cmake_output} /tmp/${cmake_name}
 
-#
-# coverage
-#
-sudo pip install cpp-coveralls==0.3.12
+sudo apt-get clean
+echo "Cleaned cache."
