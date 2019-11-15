f42be0cf40b39f5192975170827b807e80531791
Marco Craveiro <marco.craveiro@gmail.com>
2019-11-15 11:14:05 +0000

    two layers, skip empty folders: make model names smaller
    Windows MSVC builds are failing because these two models have blown
    the maximum path of 248 characters. Rename the models to a smaller
    name to  see if it fixes the issue.

---
+++ projects/cpp_ref_impl.two_layers/include/cpp_ref_impl.two_layers/hash/Package_2/class_2_hash.hpp
@@ -0,0 +1,51 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2015 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+#ifndef CPP_REF_IMPL_TWO_LAYERS_HASH_PACKAGE_2_CLASS_2_HASH_HPP
+#define CPP_REF_IMPL_TWO_LAYERS_HASH_PACKAGE_2_CLASS_2_HASH_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <functional>
+#include "cpp_ref_impl.two_layers/types/Package_2/class_2.hpp"
+
+namespace cpp_ref_impl::two_layers::Package_2 {
+
+struct class_2_hasher {
+public:
+    static std::size_t hash(const class_2& v);
+};
+
+}
+
+namespace std {
+
+template<>
+struct hash<cpp_ref_impl::two_layers::Package_2::class_2> {
+public:
+    size_t operator()(const cpp_ref_impl::two_layers::Package_2::class_2& v) const {
+        return cpp_ref_impl::two_layers::Package_2::class_2_hasher::hash(v);
+    }
+};
+
+}
+#endif
